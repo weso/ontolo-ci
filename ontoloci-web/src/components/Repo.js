c83f5272898e9a6ef2eed44e04709bb682bddbde
Pablo Menéndez Suárez <uo252406@uniovi.es>
2020-10-08 14:28:59 +0200

    builds

--- ontoloci-web/src/components/Repo.js
+++ ontoloci-web/src/components/Repo.js
@@ -19,15 +19,14 @@ function Repo(props){
             <svg xmlns="http://www.w3.org/2000/svg" className={"icon-check-"+getPassClass()} height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0z" fill="none"/><path d="M9 16.17L4.83 12l-1.42 1.41L9 19 21 7l-1.41-1.41z"/></svg>
         </div>
         <div className="repo-info">
-            <a className={"repo-"+getPassClass()}>{props.owner}</a>
-            <a className={"repo-"+getPassClass()}>{props.repo}</a>
+            <a className={getPassClass()}>{props.owner}</a>
+            <a className={getPassClass()}>{props.repo}</a>
         </div>
 
         <div className="repo-info">
-
             <a className="commit-name">{props.commitName}</a>
-            <div className="branch-container">
-                <div className="branch-logo">
+            <div className="logo-container">
+                <div className="logo">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 17 17" className="icon"><circle cx="4.94" cy="2.83" r="1.83" fill="none" stroke="#9d9d9d" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10"></circle><circle cx="11.78" cy="5.15" r="1.83" fill="none" stroke="#9d9d9d" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10"></circle><circle cx="4.98" cy="14.17" r="1.83" fill="none" stroke="#9d9d9d" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10"></circle><path d="M11.78 6.99s.09 2.68-1.9 3.38c-1.76.62-2.92-.04-4.93 1.97V4.66" fill="none" stroke="#9d9d9d" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10"></path></svg>
                 </div>
                 <a className="branch-name">{props.branchName}</a>
@@ -35,15 +34,36 @@ function Repo(props){
         </div>
 
         <div className="repo-info github-metadata">
-        
-            <a className="commit-name">{props.commitName}</a>
-            <a className="branch-name">{props.branchName}</a>
+            <div className="logo-container">
+                <div className="logo">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 17 17" className="icon"><circle cx="8.51" cy="8.5" r="3.5" fill="none" stroke="#9d9d9d" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10"></circle><path d="M16.5 8.5h-4.49m-7 0H.5" fill="none" stroke="#9d9d9d" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10"></path></svg>
+                </div>
+                <a href="#" className={getPassClass()+" pr-id"}>{props.prId}</a>
+            </div>
+            <div className="logo-container">
+                <div className="logo">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 17 17" className="icon"><circle cx="8.51" cy="8.5" r="3.5" fill="none" stroke="#9d9d9d" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10"></circle><path d="M16.5 8.5h-4.49m-7 0H.5" fill="none" stroke="#9d9d9d" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10"></path></svg>
+                </div>
+                <a className="commit-id">{props.commitId}</a>
+            </div>
         </div>
 
         <div className="repo-info ci-metadata">
-            <a className="commit-name">{props.commitName}</a>
-            <a className="branch-name">{props.branchName}</a>
+            <div className="logo-container">
+                <div className="logo">
+                <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-color" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0z" fill="none"/><path d="M15 1H9v2h6V1zm-4 13h2V8h-2v6zm8.03-6.61l1.42-1.42c-.43-.51-.9-.99-1.41-1.41l-1.42 1.42C16.07 4.74 14.12 4 12 4c-4.97 0-9 4.03-9 9s4.02 9 9 9 9-4.03 9-9c0-2.12-.74-4.07-1.97-5.61zM12 20c-3.87 0-7-3.13-7-7s3.13-7 7-7 7 3.13 7 7-3.13 7-7 7z"/></svg>
+                </div>
+                <a className="pr-id">{props.executionTime}</a>
+            </div>
+            <div className="logo-container">
+                <div className="logo">
+                <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-color" viewBox="0 0 24 24" fill="black" width="18px" height="18px"><path d="M0 0h24v24H0z" fill="none"/><path d="M19 3h-1V1h-2v2H8V1H6v2H5c-1.11 0-1.99.9-1.99 2L3 19c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm0 16H5V8h14v11zM7 10h5v5H7z"/></svg>
+                </div>
+                <a className="commit-id">{props.date}</a>
+            </div>
         </div>
+        
+   
         
     </div>);
 }
--- ontoloci-web/src/components/RepoList.js
+++ ontoloci-web/src/components/RepoList.js
@@ -11,12 +11,56 @@ function App() {
               repo="hercules-ontology" 
               pass={true}
               commitName="Ontology Update"
-              branchName="master"/>
-        <Repo owner="weso" repo="cidoc-ontology" pass={false}/>
-        <Repo owner="weso" repo="asio-ontology" pass={true}/>
-        <Repo owner="mistermboy" repo="murica-ontology" pass={true}/>
-        <Repo owner="mistermboy" repo="hercules-ontology" pass={false}/>
-        <Repo owner="mistermboy" repo="cidoc-ontology" pass={true}/>
+              branchName="master"
+              prId="#71 passed"
+              commitId="f920308"
+              executionTime="3 min 12 sec"
+              date="28 days ago"/>
+        <Repo owner="weso" 
+              repo="cidoc-ontology" 
+              pass={false}
+              commitName="Ontology Update"
+              branchName="develop"
+              prId="#71 passed"
+              commitId="f920308"
+              executionTime="3 min 12 sec"
+              date="28 days ago"/>
+        <Repo owner="weso" 
+              repo="asio-ontology" 
+              pass={true}
+              commitName="Ontology Update"
+              branchName="gh-pages"
+              prId="#71 passed"
+              commitId="f920308"
+              executionTime="3 min 12 sec"
+              date="28 days ago"/>
+        <Repo owner="mistermboy" 
+              repo="hercules-ontology" 
+              pass={true}
+              commitName="Ontology Update"
+              branchName="emilio_20%"
+              prId="#71 passed"
+              commitId="f920308"
+              executionTime="3 min 12 sec"
+              date="28 days ago"/>
+        <Repo owner="mistermboy" 
+              repo="cidoc-ontology" 
+              pass={false}
+              commitName="Ontology Update"
+              branchName="ontology_update"
+              prId="#71 passed"
+              commitId="f920308"
+              executionTime="3 min 12 sec"
+              date="28 days ago"/>
+        <Repo owner="mistermboy" 
+              repo="asio-ontology" 
+              pass={true}
+              commitName="Ontology Update"
+              branchName="master"
+              prId="#71 passed"
+              commitId="f920308"
+              executionTime="3 min 12 sec"
+              date="28 days ago"/>
       </div>
     </div>
   );
