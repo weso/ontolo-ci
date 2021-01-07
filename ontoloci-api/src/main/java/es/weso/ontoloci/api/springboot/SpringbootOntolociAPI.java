46385d1f02a5e873be099fca0f4d17b3ba0a14b8
mistermboy <pabloyo97@hotmail.com>
2021-01-07 14:16:10 +0100

    un poco más de javadoc que hay poco xd

--- ontoloci-api/src/main/java/es/weso/ontoloci/api/springboot/SpringbootOntolociAPI.java
+++ ontoloci-api/src/main/java/es/weso/ontoloci/api/springboot/SpringbootOntolociAPI.java
@@ -11,6 +11,13 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Objects;
 
+/**
+ * This class provides an endpoint for the ontoloci-web module.
+ * Allows the front to collect all the build results and for each
+ * build the result of the test cases.
+ *
+ * @author Guillermo Facundo Colunga, Pablo Menéndez
+ */
 @RestController
 @CrossOrigin( value = "*")
 @RequestMapping("/api/v1")
@@ -18,30 +25,29 @@ public class SpringbootOntolociAPI implements OntolociAPI {
 
     // LOGGER CREATION
     private static final Logger LOGGER = LoggerFactory.getLogger(SpringbootOntolociAPI.class);
-
     private final OntolociDAO persistence = OntolociInMemoryDAO.instance();
 
-
+    /**
+     * API endpoint for fetching all the build results.
+     * @return build results
+     */
     @GetMapping("/buildResults")
     @Override
     public List<PersistedBuildResult> getAllBuildResults() {
-        LOGGER.debug(
-                String.format(
-                        "GET '/buildResults' endpoint triggered, returning [%s] elements",
-                        persistence.findAllBuildResults().size()
-                )
-        );
-
+        LOGGER.debug(String.format("GET '/buildResults' endpoint triggered, returning [%s] elements", persistence.findAllBuildResults().size()));
         return persistence.findAllBuildResults();
     }
 
-
+    /**
+     * API endpoint for fetching a concrete build result by an ID.
+     * This ID is defined in the request path parameters.
+     *
+     * @return build result
+     */
     @GetMapping("/buildResults/{buildId}")
     @Override
     public PersistedBuildResult getBuildResult(@PathVariable String buildId) {
-        LOGGER.debug(
-                String.format("GET '/buildResults/{buildId}' endpoint triggered, with buildId=[%s]", buildId)
-        );
+        LOGGER.debug(String.format("GET '/buildResults/{buildId}' endpoint triggered, with buildId=[%s]", buildId));
 
         final PersistedBuildResult result = persistence.findBuildResultForId(buildId).orElse(PersistedBuildResult.from());
 
@@ -49,7 +55,6 @@ public class SpringbootOntolociAPI implements OntolociAPI {
         if(Objects.isNull(result.getId())) {
             LOGGER.error(String.format("no result found for buildId=[%s]", buildId));
         }
-
         return result;
     }
 
