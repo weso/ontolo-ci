f9a6b238c1fc3a886ff953f71667dec15fc7278a
mistermboy <pabloyo97@hotmail.com>
2021-01-15 15:41:15 +0100

    [FRONT] Shorted Builds again

--- ontoloci-web/src/utils/buildUtils.js
+++ ontoloci-web/src/utils/buildUtils.js
@@ -1,12 +1,14 @@
 export function sortBuilds(unsortedBuilds){
     return unsortedBuilds.sort((build1,build2)=>{
-      if(build1.id > build2.id ){
-        return -1;
-      }
-      if(build1.id < build2.id ){
-        return 1;
-      }
+      let date1 = build1.metadata.execution_date;
+      let date2 = build2.metadata.execution_date;
 
+      if(date1 > date2 )
+        return -1;
+    
+      if(date1< date2 )
+        return 1;
+    
       return 0;
     })
 }
